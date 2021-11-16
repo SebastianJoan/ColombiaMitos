bf72c7a8c35868692f7b8b3de9a6b2bad8a76b58
Joan Sebastián Giraldo Morales <sebastiangiraldo3@gmail.com>
2021-11-16 11:29:01 -0500

    Actualización MainPage
    Actualización diseño pagina principal

--- lib/src/MainPage.dart
+++ lib/src/MainPage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
@@ -7,8 +8,51 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return SafeArea(
-      child: Container(),
+      child: Scaffold(
+        backgroundColor: const Color.fromRGBO(27, 27, 27, 1),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Text(
+                  'Bienvenido a Colombia Mitos',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:20.0),
+                child: Container(
+                  width: size.width*0.90,
+                  height: size.height*0.20,
+                  color: Colors.red,
+                  child: const Padding(
+                    padding:  EdgeInsets.all(20.0),
+                    child:  Text(
+                      'Colombia mitos consiste en lo siguiente..................................ssssssssssssssssssssssssssss',
+                      overflow: TextOverflow.visible,
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
